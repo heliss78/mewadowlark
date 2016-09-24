afeb8067ae18082e1f0bfe3355d2695c11e3176d
heliss78 <heliss78@gmail.com>
2016-09-23 21:32:34 -0400

    Moved 'fortunen cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var fortune = require('./lib/fortune.js');
+
 var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -8,13 +10,6 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.Port || 3000);
 app.use(express.static(__dirname + '/public'));
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
 
 app.get('/', function(req,res) {
     res.render('home');
@@ -22,9 +17,9 @@ app.get('/', function(req,res) {
 });
 
 app.get('/about',function(req,res) {
-    var randomFortune =
-        fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune: randomFortune});
+   // var randomFortune =
+       // fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 
